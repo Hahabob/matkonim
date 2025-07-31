474181ab6ef5fdb58984be5ad7e459c6243a04df
Ori Hadad <orisor101@gmail.com>
2025-07-31 16:38:44 +0300

    fixed fetchuser

--- Clinet/src/context/AuthContext.tsx
+++ Clinet/src/context/AuthContext.tsx
@@ -30,8 +30,8 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       const { data } = await api.get("/auth/me", {
         withCredentials: true,
       });
-      setUser(data.user);
-      console.log("AuthContext: user data fetched:", data.user);
+      setUser(data.data);
+      console.log("AuthContext: user data fetched:", data.data);
     } catch (err) {
       console.error("AuthContext: error fetching user:", err);
       setUser(null);
