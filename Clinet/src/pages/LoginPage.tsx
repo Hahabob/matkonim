5b0e8063a86687ae95f59e1e8b89379d0edf88d9
hahabob <orisor101@gmail.com>
2025-08-01 13:15:40 +0300

    added auth context to the loginpage

--- Clinet/src/pages/LoginPage.tsx
+++ Clinet/src/pages/LoginPage.tsx
@@ -1,8 +1,18 @@
 import { LoginForm } from "@/components/ui/Forms/loginForm";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/home", { replace: true });
+    }
+  }, [user, navigate]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-6 px-4">
       <LoginForm />
